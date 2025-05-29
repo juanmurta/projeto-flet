288466500e311b3160b181df2333d6a4d2c0cbbb
juanmurta <juanmurta@gmail.com>
2025-05-29 15:00:51 -0300

    Funções de chat local completo

--- main.py
+++ main.py
@@ -2,7 +2,44 @@ import flet as ft
 
 
 def main(pagina):
-    pass
+    texto = ft.Text("Chat")
+
+    chat = ft.Column()
+
+    nome_usuario = ft.TextField(label="Nome de usuario")
+
+    def enviar_mensagem(evento):
+        chat.controls.append(ft.Text(campo_mensagem.value))
+        campo_mensagem.value = ""
+        pagina.update()
+
+    campo_mensagem = ft.TextField(label="Digite uma mensagem")
+    botao_enviar_mensagem = ft.ElevatedButton("Enviar", on_click=enviar_mensagem)
+
+    def entrar_popup(evento):
+        pagina.add(chat)
+        popup.open = False
+        pagina.remove(botao_iniciar)
+        pagina.remove(texto)
+        pagina.add(ft.Row([campo_mensagem, botao_enviar_mensagem])),
+        pagina.update()
+
+    popup = ft.AlertDialog(
+        open=False,
+        modal=True,
+        title=ft.Text("Bem vindo ao Chat"),
+        content=nome_usuario,
+        actions=[ft.ElevatedButton("Entrar", on_click=entrar_popup)],
+    )
+
+    def entrar_chat(evento):
+        pagina.open(popup)
+
+    botao_iniciar = ft.ElevatedButton("Iniciar Chat", on_click=entrar_chat)
+
+    pagina.add(texto)
+    pagina.add(botao_iniciar)
 
 
 ft.app(target=main)
+
